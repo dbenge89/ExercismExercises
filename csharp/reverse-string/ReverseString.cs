a9471fc87e316a44e9c514fe952b43ee59565de2
dbenge89 <davidcbenge89@gmail.com>
2021-09-28 09:56:45 -0400

    Reverse String added and solved

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        if(input == null || input == "") return "";
+        
+        char [] arr = input.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+
+        
+        
+    }
+}
